221c72a79f84b4f4318929fa96539027a89032e3
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2023-03-13 17:55:31 -0300

    imagens e appbar da home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.grey, opacity: 1),
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          "images/youtube.png",
+          width: 98,
+          height: 22,
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.videocam)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.account_circle))
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
