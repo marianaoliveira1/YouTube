0fa04de001ee1215198b2a9cf8e7b8dc94f51b51
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2023-03-13 17:45:13 -0300

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: Container(),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
